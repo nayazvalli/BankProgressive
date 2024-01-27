f5c4e8f722e8338a82bc5d485fb8b533aab2dd59
username <wecp-dummy@gmail.com>
2024-01-27 11:20:18 +0000

    Day 1 Task

--- backend/src/main/java/com/wecp/progressive/entity/Transactions.java
+++ backend/src/main/java/com/wecp/progressive/entity/Transactions.java
@@ -3,6 +3,10 @@ import java.util.Date;
 
 
 public class Transactions {
-
+    private int transaction_id;
+    private int account_id;
+    private double amount;
+    private Date transaction_date;
+    private String transaction_type;
 
 }
