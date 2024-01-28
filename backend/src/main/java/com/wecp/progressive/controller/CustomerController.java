9a2946ce5fbd5a114ff6e8c49468c837536dd4bb
username <wecp-dummy@gmail.com>
2024-01-28 13:56:41 +0000

    day 5 try

--- backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
+++ backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
@@ -3,15 +3,27 @@ package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Customers;
 import com.wecp.progressive.entity.Transactions;
+import com.wecp.progressive.service.CustomerServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping
 
 public class CustomerController {
+    @Autowired
+    CustomerServiceImpl service;
 
     public ResponseEntity<List<Customers>> getAllCustomers() {
-        return null;
+        return new ResponseEntity<>(service.getAllCustomers(),HttpStatus.OK);
     }
 
     public ResponseEntity<Customers> getCustomerById(int customerId) {
